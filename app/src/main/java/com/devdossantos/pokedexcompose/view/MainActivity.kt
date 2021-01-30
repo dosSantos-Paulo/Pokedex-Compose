59f2564519a5172915356f629ec97b1122952ca9
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-30 19:20:27 -0300

    inserindo Imagem da API

--- app/src/main/java/com/devdossantos/pokedexcompose/view/MainActivity.kt
+++ app/src/main/java/com/devdossantos/pokedexcompose/view/MainActivity.kt
@@ -1,41 +1,41 @@
 package com.devdossantos.pokedexcompose.view
 
-import android.icu.lang.UCharacter.DecompositionType.SMALL
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.transition.Transition
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.R
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Providers
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
 import com.devdossantos.pokedexcompose.api.model.response.pokemon.PokemonModel
 import com.devdossantos.pokedexcompose.utils.GetBackgroundColor
 import com.devdossantos.pokedexcompose.viewmodel.PokeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private val _pokeViewModel: PokeViewModel by viewModel()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         getList()
@@ -60,29 +60,36 @@ class MainActivity : AppCompatActivity() {
         val context = ContextAmbient.current
         LazyColumnFor(items = pokemonList) { pokemon ->
             PokemonRow(pokemon = pokemon, onPokeClick = {
-                Toast.makeText(context, "You clicked ${pokemon.name}", Toast.LENGTH_LONG).show()
+
             })
         }
     }
 
     @Composable
     private fun PokemonRow(pokemon: PokemonModel, onPokeClick: (PokemonModel) -> Unit) {
-        var color = Color.White
+        var remember = remember { mutableStateOf(false) }
+        var favoriteColor = remember { mutableStateOf(Color.Transparent) }
+        var bitmap by remember { mutableStateOf<Bitmap?>(null)}
+
+        Glide.with(ContextAmbient.current).asBitmap()
+            .load(pokemon.sprites?.front_default)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {}
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?
+                ) {
+                    bitmap = resource
+                }
+            })
 
         Row(
             modifier = Modifier
-
+                .background(Color.LightGray)
                 .fillMaxWidth()
         ) {
 
-            val imageModifier = Modifier
-                .preferredSize(46.dp)
-                .clip(shape = CircleShape)
-            val image = imageResource(id = R.drawable.abc_ic_star_half_black_48dp)
-
             val type = pokemon.types?.get(0)?.type?.name.toString()
-
-//            Image(asset = image, modifier = imageModifier, contentScale = ContentScale.Crop)
 
 
             Card(
@@ -91,7 +98,10 @@ class MainActivity : AppCompatActivity() {
                 modifier = Modifier
                     .padding(start = 20.dp, end = 20.dp, top = 20.dp)
                     .fillMaxWidth()
-                    .clickable(onClick = { onPokeClick(PokemonModel()) })
+                    .clickable(onClick = {
+                        favoriteColor.value =
+                            favoriteMessage(remember.value, pokemon.name.toString())
+                    })
 
             ) {
                 Box(
@@ -114,10 +124,42 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
+
+                    bitmap?.let{
+                        Image(
+                            bitmap = it.asImageBitmap(),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(120.dp)
+                                .background(Color.Transparent)
+                                .align(Alignment.BottomEnd),
+                            alignment = Alignment.BottomEnd,
+                            contentScale = ContentScale.Fit,
+                            alpha = 1f,
+                            colorFilter = null
+                        )
+                    }
+
                 }
 
             }
         }
+    }
+
+
+
+    private fun favoriteMessage(it: Boolean, name: String): Color {
+        var color = Color.Transparent
+
+        if (it){
+            Toast.makeText(this, "Você desfavoriou o $name", Toast.LENGTH_LONG).show()
+            color = Color.Transparent
+        } else if (!it) {
+            Toast.makeText(this, "Você favoriou o $name", Toast.LENGTH_LONG).show()
+            color = Color.Yellow
+        }
+
+        return color
     }
 
 
